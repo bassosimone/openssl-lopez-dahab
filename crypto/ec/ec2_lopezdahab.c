f9a8ba5b81e81d8bca82ce80527487a355513d02
Simone Basso <bassosimone@gmail.com>
2012-09-01 19:56:15 +0200

    Fix attribution
    The project is jount work, Matteo will soon merge his patches, so
    just give attribution to both of us.

--- crypto/ec/ec2_lopezdahab.c
+++ crypto/ec/ec2_lopezdahab.c
@@ -14,7 +14,7 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * Written by Simone Basso.
+ * Written by Simone Basso and Matteo Avalle.
  */
 
 /*
--- tools/ec2_lopezdahab.py
+++ tools/ec2_lopezdahab.py
@@ -15,7 +15,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
-# Written by Simone Basso.
+# Written by Simone Basso and Matteo Avalle.
 #
 
 ''' Generates code for lopezdahab elliptic curves from three operand
